cb891e5f25990836a685c57a1ed88035460a5577
oneIwant <jsanchez0937@gmail.com>
2022-08-13 14:01:26 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import shark from "./shark-punch.gif";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
+  const incrementCount = () => setCount(count + 1);
+  const decrementCount = () => setCount(count - 1);
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const searchClick = () => {
+    window.open(`https://www.google.com/search?tbm=isch&q=${search}`);
+    setSearch("");
+  };
+
+  return (
+    <div className="App">
+      <img src="/img/dbz.gif" alt="fusion"></img>
+      <h2>Welcome to my Site</h2>
+      <img src={shark} alt="punch"></img>
+
+      <div>
+        <button onClick={searchClick}>Search</button>
+        <input
+          onInput={updateSearch}
+          value={search}
+          placeholder="Image Search"/>
+        <label>Image Search</label>
+      </div>
+
+      <div className="click-container">
+        <button onClick={incrementCount}>+</button>
+        <h3>Click count: {count}</h3>
+        <button onClick={decrementCount}>-</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;

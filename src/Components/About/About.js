d6942a4ddadbde6da6f6cf9c33e8f0e2b8426e3a
oneIwant <jsanchez0937@gmail.com>
2022-08-29 23:53:16 -0400

    button links

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,16 +1,26 @@
-import "./About.css"
+import "./About.css";
 
-export default function About(){
-  return <div className= "page" id ="about">
-  <h1 className="aboutMe"> About Me </h1>
-  <div className="summary">
-  An advocate of learning and gaining experience. My interest in technology was ignited in elementary school when I played The Oregon Trail on an IBM computer, since that day I have always yearned to deconstruct and improve software in computers.
+export default function About() {
+  return (
+    <div className="page" id="about">
+      <h1 className="aboutMe"> About Me </h1>
 
-I'm always eager to take on more difficult roles in the workplace, Furthermore I try to consider others perspectives to get a better understanding of their thought processes in order to create a more engaging team environment. I don't like to stay idle and try my best to stay ahead of my tasks. 
-I had a strong desire to explore software engineering to combine my partial computer skills and have a deeper understanding of what is going on behind the scenes of computer programs and applications. 
-I am now shifting towards software development under the tutelage of Nebula Academy, and  looking for an opportunity to learn and grow with a company to launch their websites or applications.
-
-  </div>
-  </div>
+      <p className="intro">
+        An advocate of learning and gaining experience. My interest in
+        technology was ignited in elementary school when I played The Oregon
+        Trail on an IBM computer, since that day I have always yearned to
+        deconstruct and improve software in computers. I'm always eager to take
+        on more difficult roles in the workplace, Furthermore I try to consider
+        others perspectives to get a better understanding of their thought
+        processes in order to create a more engaging team environment. I don't
+        like to stay idle and try my best to stay ahead of my tasks. I had a
+        strong desire to explore software engineering to combine my partial
+        computer skills and have a deeper understanding of what is going on
+        behind the scenes of computer programs and applications. I am now
+        shifting towards software development under the tutelage of Nebula
+        Academy, and looking for an opportunity to learn and grow with a company
+        to launch their websites or applications.
+      </p>
+    </div>
+  );
 }
-
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,6 +1,26 @@
-import "./Portfolio.css"
-export default function NavBar(){
-  return <div className='page'>
-  <h1 className="Port">Portfolio</h1>
-  </div>
+import "./Portfolio.css";
+import CalcImage from "../../imgs/Calc-project.png";
+import PacMimage from "../../imgs/pacman.png";
+
+export default function NavBar() {
+  return (
+    <div className="page">
+      <h1 className="Port">Portfolio</h1>
+      {/* make links open in a new page */}
+      <div className="links">
+        <a
+          className="calc"
+          href="https://john-sanchez-ii.github.io/Calculator-II/"
+        >
+          <img src={CalcImage} height="250" width="200" />
+        </a>
+        <a
+          className="pacman"
+          href="https://john-sanchez-ii.github.io/Game-Project/"
+        >
+          <img src={PacMimage} height="250" width="200" />
+        </a>
+      </div>
+    </div>
+  );
 }

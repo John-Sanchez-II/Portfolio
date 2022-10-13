a7268c67ead891259bb50aabdee87979003d734d
oneIwant <jsanchez0937@gmail.com>
2022-10-13 01:10:41 -0400

    landing Page styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,13 @@
 import "./LandingPage.css";
 
 export default function LandingPage(){
-  return <div className="page" id= "home">
-  <h1 className="home"></h1>
+  return <div className="page" >
+    <div className="wrapper">
+      <h1 className="name">Juan Sanchez</h1>
+  <p className="greeting">Develop. Style. Sleep. Repeat  </p>
+  <h2 className="hobby">Building plastic model kits in between</h2>
+
+  
+  </div>
   </div>
 }

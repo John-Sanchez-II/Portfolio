8acfffe6519615202e7b5a5983587c52225bc657
oneIwant <jsanchez0937@gmail.com>
2022-12-05 23:15:52 -0500

    styling navbar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,12 +9,7 @@ export default function About() {
       <img alt="selfie" id="self" src={selfie} />
 
       <p className="intro">
-        An advocate of learning and gaining experience. My interest in
-        technology was ignited in elementary school while waiting my turn to use
-        the one computer in the classroom, and wondering how it worked. since
-        that day I have always yearned to deconstruct and improve software in
-        computers. I'm always eager to take on more difficult roles in the
-        workplace, Furthermore I try to consider others perspectives to get a
+         I try to consider others perspectives to get a
         better understanding of their thought processes in order to create a
         more engaging team environment. I don't like to stay idle and try my
         best to stay ahead of my tasks. I had a strong desire to explore

fbadea6382a55901bfab7ec8e08d156ce35da568
oneIwant <jsanchez0937@gmail.com>
2022-12-14 20:54:08 -0500

    netlify troubleshooting

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Portfolio from "./components/Portfolio/Portfolio";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
-import { FaGithubAlt } from "react-icons/fa";
-import { SiCodewars } from "react-icons/si";
-import { AiOutlineLinkedin } from "react-icons/ai";
 
 
   
